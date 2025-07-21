ce88d65259a657b52f2e1912648911b0dac86322
LakshiVivek <lakshi.vivek@gmail.com>
2025-07-21 11:48:56 +0530

    Created new project and basic coding

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,20 @@
+package com.sample;
+
+public class ClassA {
+
+	private void webTesting() {
+		System.out.println("Selenium");
+
+	}
+
+	private void mobileTesting() {
+		System.out.println("Appium");
+
+	}
+
+	private void apiTesting() {
+		System.out.println("Postman");
+
+	}
+
+}
---
+++ target/test-classes/Features/Sample.feature
@@ -0,0 +1,6 @@
+Feature: Sample Validation
+
+  Scenario: TC01_Validation of Login page
+    Given User launch web application
+    When User enters valid username and password
+    And User clicks login button
